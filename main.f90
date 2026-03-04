d2ae748c2559c8b13dfc39cf797aad730b45afa9
Louis Nuttall <tzm513@york.ac.uk>
2026-03-04 15:12:15 +0000

    Added input for time variables and improved error outputs for user inputs

--- main.f90
+++ main.f90
@@ -12,6 +12,9 @@ program heat
     integer                         :: size
     real(kind = dp)                 :: width, dx
 
+        ! Time stores
+    real(kind = dp)                 :: dt, t, target_t
+
         ! #######################
         ! # Initialising Arrays #
         ! #######################
@@ -31,6 +34,7 @@ program heat
                     u = 0
                     exit
                 end if
+                write(*,*) trim(errmsg)
                 write(*,*) "Please enter an integer"
             end do
 
@@ -42,6 +46,7 @@ program heat
                     dx = width/real(size, kind = dp)
                     exit
                 end if
+                write(*,*) trim(errmsg)
                 write(*,*) "Please enter a float"
             end do
             exit
@@ -55,7 +60,45 @@ program heat
             dx = width/real(size, kind = dp)
             exit
         end if
-
+        write(*,*) trim(errmsg)
         write(*,*) "Please return Y/N"
     end do
+
+        ! Start at time 0
+    t = 0
+    do
+        write(*,*) "Would you like to adjust the timestep and target time? Default is a step of 0.01 seconds,&
+            ! Line truncated
+        & with a final time of 10s"
+        read(*,'(A)', iostat = ierr, iomsg = errmsg) YN
+        if ((ierr .eq. 0) .and. (index('yY', YN) .ne. 0)) then
+
+                ! Timestep input
+            do
+                write(*,*) "How long should each timestep be?"
+                read(*,'(F10.10)', iostat = ierr, iomsg = errmsg) dt
+                if (ierr .eq. 0) exit
+                write(*,*) trim(errmsg)
+                write(*,*) "Please enter a float"
+            end do
+
+                ! Target time input
+            do
+                write(*,*) "How long should the code simulate?"
+                read(*,'(F10.10)', iostat = ierr, iomsg = errmsg) target_t
+                if (ierr .eq. 0) exit
+                write(*,*) trim(errmsg)
+                write(*,*) "Please enter a float"
+            end do
+            exit
+        else if ((ierr .eq. 0) .and. (index('nN', YN) .ne. 0)) then
+
+                ! Default values
+            dt = 0.01_dp
+            target_t = 10.0_dp
+        end if
+        write(*,*) "Please return Y/N"
+    end do
+
+
 end program
